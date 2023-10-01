d92965e7b224d7ac9b679d95386af5d8876a896d
gbantique <gbantique@gmail.com>
2023-10-01 14:05:38 +0800

    added test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,8 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+let loopCount = 0
+Brickcell.init(0)
+Brickcell.ShowString("Brickcell", 0, 0)
+basic.forever(function () {
+    loopCount += 1
+    Brickcell.ShowNumber(loopCount, 0, 1)
+    basic.pause(1000)
+})
